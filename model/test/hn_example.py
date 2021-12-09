e9fbef8b9fa67e37dcffb8b98502e7223b27a685
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-12-08 22:17:25 -0600

    Enhance test hn_example structure

--- model/test/hn_example.py
+++ model/test/hn_example.py
@@ -16,12 +16,7 @@ import pandas as pd
 import geopandas as gpd
 from strenum import StrEnum
 
-# Definición de los datos geográficos
-
-GIS_FILE_NAME = '/data/gis/gadm36_HND_1.shp'
-DEPARTMENT_COL = 'NAME_1'
 STRATUM_COL = 'Estrato'
-AREA_COL = 'Área Departamento (HA)'
 
 
 class Stratum(StrEnum):
@@ -39,114 +34,134 @@ class Stratum(StrEnum):
     ITV_92_1424 = 'ITV 92-1424'
 
 
-def get_hn_df():
-    departments = [
-        'Atlántida',
-        'Choluteca',
-        'Colón',
-        'Comayagua',
-        'Copán',
-        'Cortés',
-        'El Paraíso',
-        'Francisco Morazán',
-        'Gracias a Dios',
-        'Intibucá',
-        'Islas de la Bahía',
-        'La Paz',
-        'Lempira',
-        'Ocotepeque',
-        'Olancho',
-        'Santa Bárbara',
-        'Valle',
-        'Yoro'
-    ]
+class Gis:
+    FILE_NAME = '/data/gis/gadm36_HND_1.shp'
+    DEPARTMENT_COL = 'NAME_1'
+    AREA_COL = 'Área Departamento (HA)'
 
-    strata = [
-        Stratum.CP_72_2086,
-        Stratum.NONE,
-        Stratum.MEX_79_431,
-        Stratum.ITV_92_1424,
-        Stratum.MEX_79_431,
-        Stratum.CP_72_2086,
-        Stratum.CP_72_2086,
-        Stratum.MEX_69_290,
-        Stratum.ITV_92_1424,
-        Stratum.NONE,
-        Stratum.NONE,
-        Stratum.MEX_69_290,
-        Stratum.CP_72_2086,
-        Stratum.CP_72_2086,
-        Stratum.MEX_69_290,
-        Stratum.MEX_79_431,
-        Stratum.NONE,
-        Stratum.CP_72_2086
-    ]
+    def __init__(self):
+        self.__df = Gis.get_df()
+        self.parent_dir = '.'
 
-    areas_km2 = [
-        4227,
-        4397,
-        8875,
-        5120,
-        3239,
-        3911,
-        7383,
-        8619,
-        15876,
-        3126,
-        229,
-        2534,
-        4285,
-        1636,
-        24038,
-        5013,
-        1618,
-        7787
-    ]  # https://es.wikipedia.org/wiki/Organizaci%C3%B3n_territorial_de_Honduras
+    def df(self):
+        return self.__df
 
-    areas_ha = list(map(lambda a: a * 100, areas_km2))
+    def load_gdf(self):
+        gdf_data = gpd.read_file(self.parent_dir + Gis.FILE_NAME)
+        return gpd.GeoDataFrame(self.__df, geometry=gdf_data.geometry)
 
-    return pd.DataFrame({
-        DEPARTMENT_COL: departments,
-        STRATUM_COL: strata,
-        AREA_COL: areas_ha
-    })
+    @staticmethod
+    def get_df():
+        departments = [
+            'Atlántida',
+            'Choluteca',
+            'Colón',
+            'Comayagua',
+            'Copán',
+            'Cortés',
+            'El Paraíso',
+            'Francisco Morazán',
+            'Gracias a Dios',
+            'Intibucá',
+            'Islas de la Bahía',
+            'La Paz',
+            'Lempira',
+            'Ocotepeque',
+            'Olancho',
+            'Santa Bárbara',
+            'Valle',
+            'Yoro'
+        ]
+
+        strata = [
+            Stratum.CP_72_2086,
+            Stratum.NONE,
+            Stratum.MEX_79_431,
+            Stratum.ITV_92_1424,
+            Stratum.MEX_79_431,
+            Stratum.CP_72_2086,
+            Stratum.CP_72_2086,
+            Stratum.MEX_69_290,
+            Stratum.ITV_92_1424,
+            Stratum.NONE,
+            Stratum.NONE,
+            Stratum.MEX_69_290,
+            Stratum.CP_72_2086,
+            Stratum.CP_72_2086,
+            Stratum.MEX_69_290,
+            Stratum.MEX_79_431,
+            Stratum.NONE,
+            Stratum.CP_72_2086
+        ]
+
+        areas_km2 = [
+            4227,
+            4397,
+            8875,
+            5120,
+            3239,
+            3911,
+            7383,
+            8619,
+            15876,
+            3126,
+            229,
+            2534,
+            4285,
+            1636,
+            24038,
+            5013,
+            1618,
+            7787
+        ]  # https://es.wikipedia.org/wiki/Organizaci%C3
+        # %B3n_territorial_de_Honduras
+
+        areas_ha = list(map(lambda a: a * 100, areas_km2))
+
+        return pd.DataFrame({
+            Gis.DEPARTMENT_COL: departments,
+            STRATUM_COL: strata,
+            Gis.AREA_COL: areas_ha
+        })
 
 
-def get_hn_gdf(hn_df, rel_parent='.'):
-    gdf_data = gpd.read_file(rel_parent + GIS_FILE_NAME)
-    return gpd.GeoDataFrame(hn_df, geometry=gdf_data.geometry)
+class HardSampling:
+    YIELD_COL = 'TA/HA Real'  # Toneladas de Azúcar por Hectárea (Rendimiento)
+    HARVESTED_AREA_COL = 'Área Cosechada (HA)'
+    POPULATION_SIZE = 100_000
 
-
-# Definición de los datos del muestreo físico
-
-YIELD_COL = 'TA/HA Real'  # Toneladas de Azúcar por Hectárea (Rendimiento)
-HARVESTED_AREA_COL = 'Área Cosechada (HA)'
-POPULATION_SIZE = 100_000
-
-
-def generate_psampling():
-    """
-    Genera el conjunto de datos correspondientes a un año a partir de
-    valores aleatorios con cierto criterio de acuerdo al problema planteado.
-    """
-    variety_data = pd.DataFrame(
-        random.choices(list(Stratum), k=POPULATION_SIZE), columns=[STRATUM_COL]
-    )
-    area_data = pd.DataFrame(
-        np.random.randint(0.1, 50_000, (POPULATION_SIZE, 1)) / 10,
-        columns=[HARVESTED_AREA_COL]
-    )
-    yield_data = pd.DataFrame(
-        np.random.randint(0, 100, (POPULATION_SIZE, 1)) / 10,
-        columns=[YIELD_COL]
-    )
-    data = [variety_data, area_data, yield_data]
-    return functools.reduce(
-        lambda left, right: pd.merge(
-            left,
-            right,
-            left_index=True,
-            right_index=True
-        ),
-        data
-    )
+    @staticmethod
+    def generate():
+        """
+        Genera el conjunto de datos correspondientes a un año a partir de
+        valores aleatorios con cierto criterio de acuerdo al problema planteado.
+        """
+        variety_data = pd.DataFrame(
+            random.choices(
+                list(Stratum),
+                k=HardSampling.POPULATION_SIZE
+            ),
+            columns=[STRATUM_COL]
+        )
+        area_data = pd.DataFrame(
+            np.random.randint(
+                0.1,
+                50_000,
+                (HardSampling.POPULATION_SIZE, 1)
+            ) / 10,
+            columns=[HardSampling.HARVESTED_AREA_COL]
+        )
+        yield_data = pd.DataFrame(
+            np.random.randint(0, 100, (HardSampling.POPULATION_SIZE, 1)) / 10,
+            columns=[HardSampling.YIELD_COL]
+        )
+        data = [variety_data, area_data, yield_data]
+        return functools.reduce(
+            lambda left, right: pd.merge(
+                left,
+                right,
+                left_index=True,
+                right_index=True
+            ),
+            data
+        )
