44e358e1eca70d1d789b10e6b95f3079a62578a0
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-12-09 01:24:57 -0600

    Add basic structure to module sampling

--- model/sampling.py
+++ model/sampling.py
@@ -8,3 +8,43 @@
 #  This source code is licensed under the BSD-3-Clause License found in the
 #  LICENSE-BSD file in the root directory of this source tree or at
 #  https://opensource.org/licenses/BSD-3-Clause.
+
+"""
+Contiene la implementación del modelo de muestreo virtual para suelo agrícola.
+
+Author: Tobias Briones
+"""
+
+DEF_STRATUM_SAMPLING_SIZE = 1_000
+
+
+class Sampling:
+    """
+    Provee una implementación del muestreo aleatorio simple estratificado.
+    """
+
+    @staticmethod
+    def from_main(main):
+        return Sampling(main.gdf(), main.df())
+
+    def __init__(self, gdf, df):
+        self.__gdf = gdf  # Immutable
+        self.__df = df  # Immutable
+        self.__n = DEF_STRATUM_SAMPLING_SIZE
+        self.__stratum_col = 'Stratum'
+
+    def stratum_sampling_size(self, n):
+        self.__n = n
+
+    def stratum_col(self, col):
+        self.__stratum_col = col
+
+    def run(self):
+        return Result()
+
+
+class Result:
+    """Define un muestreo virtual"""
+
+    def __init__(self):
+        pass
