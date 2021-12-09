7cdaf3c724292e07017e17ac4d541cc2d1d3f35b
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-12-08 21:37:02 -0600

    Create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2021 Tobias Briones. All rights reserved.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  This file is part of Course Project at UNAH-MM700: Agricultural Soil Sampling
+#  for Data Analysis.
+#
+#  This source code is licensed under the BSD-3-Clause License found in the
+#  LICENSE-BSD file in the root directory of this source tree or at
+#  https://opensource.org/licenses/BSD-3-Clause.
